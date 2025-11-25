1b07c3744abedb70c2a83431d79549af599469b5
mithlesh-vishwakarma <mv9898733607@gmail.com>
2025-11-25 11:26:37 +0530

    all three homework-practice

---
+++ c language/profile.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main (){
+
+printf("=======================================================================\n"
+        "              PERSONAL PROFILE         \n"
+        "======================================================================\n"
+        "Name       : Mithlesh Vishwakarma\n"
+        "DOB        : 16-Jan-1999\n"
+        "Location   : Surat, Gujarat, India\n"
+        "Occupation : Web developer / Entrepreneur\n"
+        "Skills     : HTML, CSS, JavaScript, React, Node.js, MongoDB, Python\n"
+        "Interests  : Reading, Sketching, Calisthenics\n"
+        "Email      : mithlesh9427@gmail.com\n"
+        "Phone      : +91-9327832747\n"
+        "Website    : https://ordinarycoder.com\n"
+        "-----------------------------------------------------------------------\n"
+        "Bio        : Energetic full-stack dev building\n"
+        "             e-commerce and manufacturing apps.\n"
+        "             Focused on learning, freelancing,\n"
+        "             and building passive income.\n"
+        "========================================================================\n");
+
+}
