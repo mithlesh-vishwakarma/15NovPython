2040aa5fda4e3c9746ccae98718faf0a6bec5cb9
mithlesh-vishwakarma <mv9898733607@gmail.com>
2025-11-27 16:35:50 +0530

    `Added additional input prompts and scanf statements to inputOutput.c`

--- c language/inputOutput.c
+++ c language/inputOutput.c
@@ -5,14 +5,15 @@ int main()
     int number;
     char ch;
     float number2;
+    
+    printf("Please enter a character to print\n");
+    scanf(" %c", &ch);            //problem found in runtime
+    printf("character = %c\n", ch);
 
     printf("Please enter a number to print\n");
     scanf("%d", &number);
     printf("number = %d\n", number);
 
-    printf("Please enter a character to print\n");
-    scanf(" %c", &ch);            //problem found in runtime
-    printf("character = %c\n", ch);
 
     printf("Please enter a float value to print\n");
     scanf("%f", &number2);      // now in float it is not asking value direct printing 0.000
