9e82b3f1ac0c933a7577744b0fa879d0997b7f3b
Deepak Sharma <mv9898733607@gmail.com>
2025-12-11 14:55:11 +0530

    updated today's work

--- c language/Class_Work_Practicals/pointers.c
+++ c language/Class_Work_Practicals/pointers.c
@@ -5,7 +5,7 @@ int main()
   int *ptr;
   ptr = &a;
 
-  printf("the Address: %c and the Value: %d", ptr, *ptr);
+  printf("the Address: %p and the Value: %d", ptr, *ptr);
   *ptr = 100;
   printf("\n a=%d", a);
   return 0;
