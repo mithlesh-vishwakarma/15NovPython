983cbcdd8481573b9b0686cfffbdf9b05e8a0531
mithlesh-vishwakarma <mv9898733607@gmail.com>
2026-06-06 13:34:46 +0530

    feat: implement AJAX-based dynamic dependent dropdowns and basic HTML structure for class assignments

--- Python/ClassWorkPracticals/018_AJAX/myappAJAX/urls.py
+++ Python/ClassWorkPracticals/018_AJAX/myappAJAX/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from myappAJAX.views import *
 
 urlpatterns = [
-    path('',index,name="index"),
-    path("test",test,name="test")
+    path("", index, name="index"),
+    path("test", test, name="test"),
+    path("search", search, name="search"),
+    path("countries", get_countries, name="countries"),
+    path("states", get_states, name="states"),
+    path("cities", get_cities, name="cities"),
 ]
--- Python/ClassWorkPracticals/018_AJAX/myappAJAX/views.py
+++ Python/ClassWorkPracticals/018_AJAX/myappAJAX/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from myappAJAX.urls import *
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from myappAJAX.models import *
 # Create your views here.
 
 def index(request):
@@ -9,4 +9,35 @@ def index(request):
 
 def test(request):
     q=request.GET['q']
-    return HttpResponse(request,f"hello,{q},how are you")
+    return HttpResponse(f"hello,{q},how are you")
+
+def search(request):
+    q=request.GET['q']
+    result=""
+    # if q=='electric':
+    #     result= "<ul><li>option1</li><li>option2</li><li>option3</li></ul>"
+
+    products=Products.objects.filter(name__startswith=q)
+    result="<ul>"
+    for product in products:
+        result+=f"<li>{product.name}</li>"
+    result+="</ul>"
+
+    return HttpResponse(result)    
+
+def get_countries(request):
+    countries=Country.objects.all()
+    return JsonResponse({"data":list(countries.values())})
+
+
+def get_states(request):
+    country_id=request.GET['country_id']
+
+    states=State.objects.filter(country_id=country_id)
+    return JsonResponse({"data":list(states.values())})
+
+
+def get_cities(request):
+    state_id = request.GET['state_id']
+    cities = City.objects.filter(state_id=state_id)
+    return JsonResponse({"data": list(cities.values())})
