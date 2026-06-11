c934e69a782f3b55810ab3996260c0eb91d027e4
mithlesh-vishwakarma <mv9898733607@gmail.com>
2026-06-11 09:38:39 +0530

    feat: implement AJAX-based CRUD operations for student management system

--- Python/ClassWorkPracticals/020_Ajax_CRUD/myapp/models.py
+++ Python/ClassWorkPracticals/020_Ajax_CRUD/myapp/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-  name = models.CharField(max_length=20)
-  email = models.CharField(max_length=50)
-  phone = models.CharField(max_length=10)
-  age = models.IntegerField()
+    name = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    age = models.IntegerField()
 
 
 def __str__(self):
--- Python/ClassWorkPracticals/020_Ajax_CRUD/myapp/views.py
+++ Python/ClassWorkPracticals/020_Ajax_CRUD/myapp/views.py
@@ -1,8 +1,58 @@
 from django.shortcuts import render
-from myapp.urls import *
-
+from django.http import JsonResponse,HttpResponse
+from myapp.models import *
 # Create your views here.
-
 def index(request):
     return render(request,'index.html')
+
+def display(request):
+    students = Student.objects.all()
+    return JsonResponse({"data":list(students.values())})
     
+    
+def search(request):
+    q = request.GET['q']
+    students = Student.objects.filter(name__startswith=q) or Student.objects.filter(email__startswith=q) or Student.objects.filter(phone__startswith=q) or Student.objects.filter(age__startswith=q)
+    
+    return JsonResponse({"data":list(students.values())})
+
+def create(request):
+    if request.method=='POST':
+        data = request.POST
+        name = data.get("name")
+        email = data.get("email")
+        phone = data.get("phone")
+        age = data.get("age")
+        
+        Student.objects.create(name=name,email=email,phone=phone,age=age)
+    return HttpResponse("Registration successfully !!!")
+
+
+def destroy(request):
+    id = request.GET['id']
+    student = Student.objects.get(id=id)
+    student.delete()
+    return HttpResponse("Student deleted !!!")
+
+
+def retrive(request):
+    id = request.GET['id']
+    student = Student.objects.filter(id=id)
+    return JsonResponse({"data":list(student.values())})
+
+def update(request):
+    if request.method=='POST':
+        data = request.POST
+        id = data.get("id")
+        name = data.get("name")
+        email = data.get("email")
+        phone = data.get("phone")
+        age = data.get("age")
+        
+        student = Student.objects.get(pk=id)
+        student.name = name
+        student.email=email
+        student.phone = phone
+        student.age = age        
+        student.save()
+    return HttpResponse("Update successfully !!!")
