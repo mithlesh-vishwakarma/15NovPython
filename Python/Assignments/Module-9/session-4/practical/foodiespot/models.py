6b1e584280a47ee546edb6d6a920c012d56c843d
mithlesh-vishwakarma <mv9898733607@gmail.com>
2026-06-19 12:16:59 +0530

    feat: implement Restaurant and Cuisin models and register them in the admin site

--- Python/Assignments/Module-9/session-4/practical/foodiespot/models.py
+++ Python/Assignments/Module-9/session-4/practical/foodiespot/models.py
@@ -1,5 +1,4 @@
-from django.db.models import ForeignKey
-from django.db.models import CharField
+
 from django.db import models
 
 # Create your models here.
@@ -8,12 +7,10 @@ class Restaurant(models.Model):
   location=models.CharField(max_length=100)
   rating=models.FloatField()
   
-  def __str__(self):
-    return self.name
+  
 
 class Cuisin(models.Model):
   restaurant=models.ForeignKey("Restaurant", on_delete=models.CASCADE)
   name=models.CharField(max_length=20)
   description=models.TextField(max_length=100)
-  def __str__(self):
-    return self.name
+ 
