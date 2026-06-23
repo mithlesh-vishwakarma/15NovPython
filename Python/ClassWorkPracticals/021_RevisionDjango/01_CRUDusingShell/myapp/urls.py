0d1df6fb1160f98394707dedf519a95cf32ba24c
mithlesh-vishwakarma <mv9898733607@gmail.com>
2026-06-23 14:00:31 +0530

    feat: implement CRUD functionality for Student model with display, update, and delete features

--- Python/ClassWorkPracticals/021_RevisionDjango/01_CRUDusingShell/myapp/urls.py
+++ Python/ClassWorkPracticals/021_RevisionDjango/01_CRUDusingShell/myapp/urls.py
@@ -3,5 +3,8 @@ from myapp.views import *
 urlpatterns=[
 
   path("",index,name="index"),
-  path("add_student",add_student,name="add_student")
+  path("add_student",add_student,name="add_student"),
+  path("display",display,name="display"),
+  path("delete",delete,name="delete"),
+  path("update",update,name="update")
 ]
