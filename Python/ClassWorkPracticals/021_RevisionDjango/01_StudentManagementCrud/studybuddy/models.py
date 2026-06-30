5d1a16409c902c7dc7b625f17f55a96ef74d50b9
mithlesh-vishwakarma <mv9898733607@gmail.com>
2026-06-30 09:34:13 +0530

    feat: implement student management system with CRUD operations, media handling, and templates

--- Python/ClassWorkPracticals/021_RevisionDjango/01_StudentManagementCrud/studybuddy/models.py
+++ Python/ClassWorkPracticals/021_RevisionDjango/01_StudentManagementCrud/studybuddy/models.py
@@ -1,38 +1,28 @@
 from django.db import models
 
-# Create your models here.
-# | Field           | Type            |
-# | --------------- | --------------- |
-# | id              | AutoField       |
-# | student_id      | CharField       |
-# | first_name      | CharField       |
-# | last_name       | CharField       |
-# | gender          | ChoiceField     |
-# | date_of_birth   | DateField       |
-# | email           | EmailField      |
-# | phone           | CharField       |
-# | address         | TextField       |
-# | city            | CharField       |
-# | state           | CharField       |
-# | course          | ForeignKey      |
-# | admission_date  | DateField       |
-# | profile_picture | ImageField      |
-# | status          | Active/Inactive |
 class Course(models.Model):
   name=models.CharField(max_length=30)
-  duration=models.models.CharField(max_length=50)
+  duration=models.CharField(max_length=50)
   fees=models.DecimalField(max_digits=10,decimal_places=2)
 
+  def __str__(self):
+      return self.name
+
+
+class Subject(models.Model):
+  name=models.CharField(max_length=20)
+  course=models.ManyToManyField(Course)
+
+  def __str__(self):
+    return self.name
+
 class Student(models.Model):
-  id=models.AutoField()
+  profile=models.ImageField(upload_to="image",null=True)
   student_id=models.CharField(max_length=12)
   first_name=models.CharField(max_length=20)
   last_name=models.CharField(max_length=20)
-  gender=models.ChoiceField()
   date_of_birth=models.DateField()
   email=models.EmailField()
   phone=models.CharField(max_length=10)
-  address=models.TextField()
-  city=models.CharField(max_length=20)
-  state=models.CharField(max_length=20)
-  course=models.ForeignKey()
+  course=models.ForeignKey(Course,on_delete=models.CASCADE)
+  admission_date=models.DateField()
