05379ee9504bf886cc25bc337e1a9db134d89d04
OrdinaryCoder <mv9898733607@gmail.com>
2026-09-11 12:11:58 +0530

    feat: implement Django form CRUD operations and add advanced JavaScript async notes

--- Python/ClassWorkPracticals/028_DjangoForms/myapp/urls.py
+++ Python/ClassWorkPracticals/028_DjangoForms/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from myapp.views import stundentData
+from myapp.views import *
 
 urlpatterns = [
-    path('',stundentData,name="stundentData")
+    path('',stundentData,name="stundentData"),
+    path('delete',delete,name="delete"),
+    path ('update',update,name="update"),
 ]
--- Python/ClassWorkPracticals/028_DjangoForms/myapp/views.py
+++ Python/ClassWorkPracticals/028_DjangoForms/myapp/views.py
@@ -1,14 +1,34 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from myapp.forms import StudentForm
+from myapp.models import Student
 
 def stundentData(request):
     form = StudentForm()
+    students = Student.objects.all()
     if request.method == "POST":
         form = StudentForm(request.POST)
         if form.is_valid():
             form.save()
-            return render(request,"index.html",{'msg':'Record Inserted Successfully'})
-        else:
-            return render(request,"index.html",{'msg':'Record Not Inserted'})    
-    return render(request,"index.html",{'form':form})
-            
+            return redirect("stundentData")
+    
+    return render(request,"index.html",{'form':form,'students':students})
+
+
+def delete(request):
+    sid = request.GET['id']
+    Student.objects.filter(id=sid).delete()
+    return redirect("stundentData")
+
+
+def update(request):
+    sid = request.GET['id']
+    student = Student.objects.get(id=sid)
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+    else:
+        form = StudentForm(instance=student)
+        
+    return render(request, "index.html", {'form': form, 'student': student})
